1e081d095760a8e30912c415e8c586048ac0fa41
sranifp <147374144+sranifp@users.noreply.github.com>
2024-06-28 20:09:13 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * -----------------------------------------------------
+ * ES234211 - Programming Fundamental
+ * Genap - 2023/2024
+ * Group Capstone Project: Snake and Ladder Game
+ * -----------------------------------------------------
+ * Class    : D
+ * Group    : 13
+ * Members  :
+ * 1. 5026221044 - Sintiarani Febyan Putri
+ * 2. 5026231035 - Aldani Prasetyo
+ * 3. 5026231160 - Muhammad Gandhi Taqi Utomo
+ * ------------------------------------------------------
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        SnL g1 = new SnL(100);
+        g1.play();
+    }
+}
